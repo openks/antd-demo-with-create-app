8a282ffeadc1d3aaa20811c8cfe9a9a8f1ab17af
openks <zhuyangxing@foxmail.com>
2017-12-25 14:21:39 +0800

    在about页添加antd的按钮

--- src/demo/about.js
+++ src/demo/about.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Button } from 'antd';
 const About = () => (
   <div>
     <h2>About</h2>
+    <Button type="primary">Button</Button>
   </div>
 )
 export default About
